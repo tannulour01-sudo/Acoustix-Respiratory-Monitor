29cbc2b118a09ab868efe4944e6cec0b19165b97
Tannu Lour <tannulour01@gmail.com>
2026-03-05 16:16:04 +0530

    Delete preprocess_audio.py

--- preprocess_audio.py
+++
@@ -1,8 +0,0 @@
-import librosa
-import numpy as np
-
-def get_mfcc_features(file):
-    # Stage 3: Feature Extraction (MFCC) [cite: 55]
-    y, sr = librosa.load(file, sr=16000)
-    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
-    return np.mean(mfccs.T, axis=0)
