0adb1152b5982f82ad303b1019b179bdf196fa69
Tannu Lour <tannulour01@gmail.com>
2026-03-05 16:02:13 +0530

    Create audio_config.h

---
+++ Firmware_ESP32/audio_config.h
@@ -0,0 +1,14 @@
+#ifndef AUDIO_CONFIG_H
+#define AUDIO_CONFIG_H
+
+// INMP441 Pins [cite: 67]
+#define I2S_WS 15
+#define I2S_SD 32
+#define I2S_SCK 14
+
+// Output Pins [cite: 67]
+#define OLED_SCL 22
+#define OLED_SDA 21
+#define BUZZER_PIN 13
+
+#endif
