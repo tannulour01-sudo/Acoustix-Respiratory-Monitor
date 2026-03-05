ab4335a6f993e88db65be9b2ce8861f271e6d407
Tannu Lour <tannulour01@gmail.com>
2026-03-05 16:07:08 +0530

    Create inference_engine.cpp

---
+++ Firmware_ESP32/inference_engine.cpp
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+
+float run_inference(float* input_data) {
+    // Stage 4: TinyML Inference [cite: 61]
+    // Classification as "Cough" or "Ambient Noise" [cite: 62]
+    float prediction = 0.90; 
+    return prediction;
+}
